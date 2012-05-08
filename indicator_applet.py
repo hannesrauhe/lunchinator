a0d22d3ba667d511822790d8b89aa997d2123cd1
Hannes Rauhe <hannes@scitivity.net>
2012-05-08 09:56:58 +0200

    windoooof

--- indicator_applet.py
+++ indicator_applet.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Canonical Ltd.
-#
-# Authors: Neil Jagdish Patel <neil.patel@canonical.com>
-#          Jono Bacon <jono@ubuntu.com>
-#
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of either or both of the following licenses:
-#
-# 1) the GNU Lesser General Public License version 3, as published by the 
-# Free Software Foundation; and/or
-# 2) the GNU Lesser General Public License version 2.1, as published by 
-# the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the applicable version of the GNU Lesser General Public 
-# License for more details.
-#
-# You should have received a copy of both the GNU Lesser General Public 
-# License version 3 and version 2.1 along with this program.  If not, see 
-# <http://www.gnu.org/licenses/>
-#
 import sys
 import gobject
 import gtk
