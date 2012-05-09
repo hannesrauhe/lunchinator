248fa973a80e17a1a556bbfd00d1b6acdd1539bc
Hannes Rauhe <hannes@scitivity.net>
2012-05-09 15:13:14 +0200

    update on start ;-)

--- start_notifier.sh
+++ start_notifier.sh
@@ -4,4 +4,6 @@
 #execute right script depending on distribution here
 #echo $DISTRIB_DESCRIPTION
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+git pull
 unbuffer $DIR/indicator_applet.py >> $HOME/.lunch_calls
