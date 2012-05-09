bb2d59fcdb797294c9222fd75e75230d6ea85792
Hannes Rauhe <hannes@scitivity.net>
2012-05-09 15:57:57 +0200

    renamed update script

--- start_notifier.sh
+++ start_notifier.sh
@@ -16,7 +16,7 @@ else
  	git add lunch_members
 	git commit -m "added $(hostname) automatically"
 	git push
-	python lunch_updater
+	python lunch_updater.py
 fi
 
 unbuffer $DIR/indicator_applet.py >> $HOME/.lunch_calls
