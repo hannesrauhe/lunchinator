1e1d63574342de8c07fc74b490aae9d110641d1f
Hannes Rauhe <hannes@scitivity.net>
2012-05-09 15:45:42 +0200

    automatically adding members on start :-)

--- start_notifier.sh
+++ start_notifier.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+
 . /etc/*release
 
 #execute right script depending on distribution here
 #echo $DISTRIB_DESCRIPTION
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+
 git pull
+
+if grep -q $(hostname) lunch_members
+	then echo "already a lunch member"
+else
+	echo $(hostname) >> lunch_members
+ 	git add lunch_members
+	git commit -m "added $(hostname) automatically"
+	git push
+fi
+
 unbuffer $DIR/indicator_applet.py >> $HOME/.lunch_calls
+
+
