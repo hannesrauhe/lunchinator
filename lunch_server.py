eee24d38bb89a525d0095ea2cf0e1ce24946913e
Hannes Rauhe <hannes@scitivity.net>
2012-05-22 15:27:12 +0200

    say helo - helo

--- lunch_server.py
+++ lunch_server.py
@@ -5,26 +5,32 @@ from time import gmtime, strftime, localtime
 import sys
 import os
 import ctypes
+import lunch_client
+import getpass
 
 class lunch_server(object):
     running = False
     auto_update = True
     new_msg = False
     last_messages = [("start","start","start")]
+    members = {"127.0.0.1":"myself"}
     
     def incoming_call(self,msg,addr):
         t = strftime("%a, %d %b %Y %H:%M:%S", localtime())
-        print "%s: [%s] %s" % (t,addr, msg)
+        m = addr
+        if addr in self.members:
+            m = self.members[addr]
+        print "%s: [%s] %s" % (t,m, msg)
         
-        self.last_messages.append((t,addr,msg))
+        self.last_messages.append((t,m,msg))
         if len(self.last_messages)>5:
             self.last_messages.pop(0)
         self.new_msg = True
             
         if sys.platform.startswith('linux'):
-            self.incoming_call_linux(msg,addr)
+            self.incoming_call_linux(msg,m)
         else:
-            self.incoming_call_win(msg,addr)
+            self.incoming_call_win(msg,m)
             
     def incoming_call_linux(self,msg,addr):    
         try:
@@ -67,6 +73,7 @@ class lunch_server(object):
     
     def start_server(self):
         print strftime("%a, %d %b %Y %H:%M:%S", localtime()),"Starting the lunch notifier service"
+        lunch_client.call("HELO "+getpass.getuser())
         self.running = True
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try: 
@@ -85,6 +92,10 @@ class lunch_server(object):
                             os.execlp("python","python",os.path.basename(sys.argv[0]))
                         else:
                             print "%s: %s issued an update but updates are disabled" % (t,addr)
+                            
+                    elif daten.startswith("HELO"):
+                        self.members[addr]=daten.split(" ",1)[1].strip()
+                        print self.members
                             
                     else:                            
                         self.incoming_call(daten,addr[0])
