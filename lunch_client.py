99d59f8fe05ff0159dca75b9a0a5abc645be8ffe
Hannes Rauhe <hannes@scitivity.net>
2012-10-31 14:07:51 +0100

    write messages to single hosts

--- lunch_client.py
+++ lunch_client.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python
 import socket,sys,os
 
-def call(msg):
+def call(msg,client=''):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     f = open(sys.path[0]+"/lunch_members",'r')
     print "sending",msg,"to",
-    for ip in f.readlines():
-        print ip.strip(),
-        # this command sends some data over the network on port 50000
+    if client:
+        print client
         try:
-            s.sendto(msg, (ip.strip(), 50000)) 
+            s.sendto(msg, (client.strip(), 50000)) 
         except:
             print "(Exception: hostname unknown)",
+    else:
+        for ip in f.readlines():
+            print ip.strip(),
+            try:
+                s.sendto(msg, (ip.strip(), 50000)) 
+            except:
+                print "(Exception: hostname unknown)",
     
     s.close()
     print ""
 
 if __name__ == "__main__":
     msg = "lunch"
+    client =''
     if len(sys.argv)>1:
         msg = sys.argv[1]
-    call(msg)
+    if len(sys.argv)>2:
+        client = sys.argv[2]
+    call(msg,client)
