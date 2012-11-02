c78a392d780c4b450bb7a3292ab27faccd7bb945
Hannes Rauhe <hannes@scitivity.net>
2012-11-02 10:02:12 +0100

    put myself on top of the list - muted the client, so it won't spam the logs

--- lunch_client.py
+++ lunch_client.py
@@ -8,16 +8,17 @@ def call_peer_nr(msg,peer_nr):
     found=False
     for ip in f.readlines():
         if i==peer_nr:
-            print "sending",msg,"to",ip.strip(),
+            #print "sending",msg,"to",ip.strip(),
             try:
                 s.sendto(msg, (ip.strip(), 50000)) 
                 found=True
                 break
-            except:
-                print "(Exception: hostname unknown)",
+            except:              
+                pass
+                #print "(Exception: hostname unknown)",
         i+=1
     s.close()
-    print ""
+    #print ""
     
     if not found:
         i=-1
@@ -26,23 +27,25 @@ def call_peer_nr(msg,peer_nr):
 def call(msg,client=''):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     f = open(sys.path[0]+"/lunch_members",'r')
-    print "sending",msg,"to",
+    #print "sending",msg,"to",
     if client:
         print client
         try:
             s.sendto(msg, (client.strip(), 50000)) 
         except:
-            print "(Exception: hostname unknown)",
+            pass
+            #print "(Exception: hostname unknown)",
     else:
         for ip in f.readlines():
-            print ip.strip(),
+            #print ip.strip(),
             try:
                 s.sendto(msg, (ip.strip(), 50000)) 
             except:
-                print "(Exception: hostname unknown)",
+                pass
+                #print "(Exception: hostname unknown)",
     
-    s.close()
-    print ""
+    s.close() 
+    #print ""
 
 if __name__ == "__main__":
     msg = "lunch"
@@ -50,5 +53,7 @@ if __name__ == "__main__":
     if len(sys.argv)>1:
         msg = sys.argv[1]
     if len(sys.argv)>2:
+        print client,":",
         client = sys.argv[2]
+    print msg
     call(msg,client)
