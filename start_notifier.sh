5c3417a598f4005b711b31eaac2b30fc38e95bac
Hannes Rauhe <hannes@scitivity.net>
2013-02-25 13:56:46 +0100

    *fixed the update routine in bash script

--- start_notifier.sh
+++ start_notifier.sh
@@ -8,16 +8,15 @@ cd $LUNCHINATOR_DIR
 mkdir -p $LUNCHINATOR_CONFIG_DIR
 mv *.cfg $LUNCHINATOR_CONFIG_DIR
 
-#update before starting
-git stash
-git pull
-
 #if the python script created an update file, restart it after it closes
 #create one for the first start to see if $LUNCHINATOR_CONFIG_DIR is writable
 touch $LUNCHINATOR_CONFIG_DIR/update
 
 while [ -f "$LUNCHINATOR_CONFIG_DIR/update" ]; do
     rm $LUNCHINATOR_CONFIG_DIR/update
+    #update before starting
+    git stash
+    git pull
     #execute right script depending on distribution here
     if [ "${DISTRIB_DESCRIPTION:0:6}" = "Ubuntu" ]; then
         unbuffer $LUNCHINATOR_DIR/indicator_applet.py --distrib-release=${DISTRIB_RELEASE} >> $LUNCHINATOR_CONFIG_DIR/lunch_calls.log
