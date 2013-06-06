42d3af9853b20eabb8f93d80a31b9b7727ae73b9
Hannes Rauhe <hannes@scitivity.net>
2013-06-06 14:24:44 +0200

    *start script

--- start_lunchinator.py
+++ start_lunchinator.py
@@ -1,4 +1,5 @@
-import subprocess,platform
+import subprocess,platform,os
+import lunch_default_config
 
 pythonex_wo_console = "python"
 
@@ -7,15 +8,25 @@ if platform.system()=="Windows":
     
 print "We are on",platform.system(),platform.release(),platform.version()
 
-try:
-    import gtk
+config_object = lunch_default_config()
+
+subprocess.call("git stash")
+subprocess.call("git pull")
+
+fhandle = file("update", 'a')
+fhandle.close()
+
+while os.path.exists("update"):        
+    os.remove("update")
     try:
-        #on ubuntu start the indicator
-        import appindicator
-        subprocess.Popen(["python","indicator_applet.py"])
+        import gtk
+        try:
+            #on ubuntu start the indicator
+            import appindicator
+            subprocess.Popen(["python","indicator_applet.py"])
+        except ImportError, e:
+            #start the tray icon on windows and other linxu flavors
+            subprocess.Popen([pythonex_wo_console,"gui_tray.py"])        
     except ImportError, e:
-        #start the tray icon on windows and other linxu flavors
-        subprocess.Popen([pythonex_wo_console,"gui_tray.py"])        
-except ImportError, e:
-    #start the CLI-Version if gtk is not available
-    subprocess.Popen(["python","nogui.py"])
+        #start the CLI-Version if gtk is not available
+        subprocess.call("python nogui.py")
