529f2770df6fe71b1453c7fb778b3839cf14c74b
Hannes Rauhe <hannes@scitivity.net>
2013-06-06 07:43:24 +0200

    *first commands for nogui

--- nogui.py
+++ nogui.py
@@ -5,7 +5,6 @@ import lunch_avatar
 import time
 import socket
 import threading,os
-
         
 class lunchinator_nogui(threading.Thread):
     menu = None
@@ -14,6 +13,10 @@ class lunchinator_nogui(threading.Thread):
     
     def __init__(self):           
         threading.Thread.__init__(self)
+        self.cmddict = {
+               "help": [self.print_help,"prints out this text :-)"],
+               "call": [self.ls.call_all_members,"calls for lunch"],
+               "q": [self.stop_server, "exit"]}
     
     def run(self):
         self.ls.start_server()   
@@ -25,6 +28,24 @@ class lunchinator_nogui(threading.Thread):
             print "server stopped" 
         else:
             print "server not running"
+    
+    def print_help(self):
+        for k,v in self.cmddict.iteritems():
+            print k,"-",v[1]        
+        
+    def process_cmd(self,cmdstring):
+        if cmdstring=="":
+            return
+        args = cmdstring.split()
+        cmd = args[0]
+        args.pop(0)
+        if cmd in self.cmddict:
+            if len(args):
+                self.cmddict[cmd][0](*args)
+            else:    
+                self.cmddict[cmd][0]()
+        else:
+            print "command not known"
         
 if __name__ == "__main__":
     l = lunchinator_nogui()
@@ -33,5 +54,16 @@ if __name__ == "__main__":
     cmd = ""
     while l.isAlive() and cmd not in ["exit","q","quit"]:
         cmd = raw_input(">")
-        print l.ls.get_member_info()
+        try:
+            l.process_cmd(cmd)            
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            print sys.exc_info()[1]
+        print "members:"
+        for m_ip,m_info in l.ls.get_member_info().iteritems():
+            if "name" in m_info:
+                print m_info["name"],
+            else:
+                print m_ip,
+        print
     l.stop_server()
