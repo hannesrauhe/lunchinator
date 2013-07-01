d523d0613c848c9007e934c77912e017517a2f76
Hannes Rauhe <hannes@scitivity.net>
2013-07-01 14:08:57 +0200

    *start_notifier.sh points to the new python-script...

--- start_notifier.sh
+++ start_notifier.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-#this is the bash file you can use to start the lunchinator on linux - should be replaces by start_lunchinator.py sooner or later
+#backward-compatibility you can use start_lunchinator.py instead of this script
 
-. /etc/*release
+echo "This script is deprecated. Please use start_lunchinator.py to fire up the lunchinator."
 
-LUNCHINATOR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LUNCHINATOR_CONFIG_DIR=$HOME/.lunchinator
-cd $LUNCHINATOR_DIR
-mkdir -p $LUNCHINATOR_CONFIG_DIR
-mv *.cfg $LUNCHINATOR_CONFIG_DIR
-
-#if the python script created an update file, restart it after it closes
-#create one for the first start to see if $LUNCHINATOR_CONFIG_DIR is writable
-touch $LUNCHINATOR_CONFIG_DIR/update
-
-while [ -f "$LUNCHINATOR_CONFIG_DIR/update" ]; do
-    rm $LUNCHINATOR_CONFIG_DIR/update
-    #update before starting
-    git stash
-    git pull
-    #execute right script depending on distribution here
-    if [ "${DISTRIB_DESCRIPTION:0:6}" = "Ubuntu" ]; then
-        unbuffer $LUNCHINATOR_DIR/indicator_applet.py --distrib-release=${DISTRIB_RELEASE} >> $LUNCHINATOR_CONFIG_DIR/lunch_calls.log
-    else
-        echo "starting gtk-tray instead of indicator"
-        unbuffer $LUNCHINATOR_DIR/gui_tray.py >> $LUNCHINATOR_CONFIG_DIR/lunch_calls.log
-    fi
-done
-#
-
+./start_lunchinator.py
