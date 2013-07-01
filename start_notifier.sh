bb759a8a70ba2a670238e83d280254e5a36f908c
Hannes Rauhe <hannes@scitivity.net>
2013-07-01 14:39:26 +0200

    *probably threw a lot of users out because I forgot to switch to the working-directory in the bash script

--- start_notifier.sh
+++ start_notifier.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #backward-compatibility you can use start_lunchinator.py instead of this script
 
-echo "This script is deprecated. Please use start_lunchinator.py to fire up the lunchinator."
+#echo "This script is deprecated. Please use start_lunchinator.py to fire up the lunchinator."
+
+LUNCHINATOR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LUNCHINATOR_CONFIG_DIR=$HOME/.lunchinator
+cd $LUNCHINATOR_DIR
+mkdir -p $LUNCHINATOR_CONFIG_DIR
 
 ./start_lunchinator.py
