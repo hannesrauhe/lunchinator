e5c1b1ded82d920c12e1528581306db0bd6b0640
Hannes Rauhe <hannes@scitivity.net>
2013-08-08 17:10:13 +0200

    *TDTnotify - rotation added :-)

--- plugins/tdtnotify/__init__.py
+++ plugins/tdtnotify/__init__.py
@@ -2,14 +2,19 @@ from lunchinator.iface_plugins import *
 import subprocess, sys, ctypes
 from yapsy.PluginManager import PluginManagerSingleton
 
-import urllib2, tempfile, json
+import urllib2, tempfile, json, time
 
 class tdtnotify(iface_called_plugin):    
     def __init__(self):
         super(tdtnotify, self).__init__()
         manager = PluginManagerSingleton.get()
         self.ls = manager.app
-        self.options = {"icon_file":sys.path[0]+"/images/mini_breakfast.png","blog_name":"tittendestages","trigger_word":""}
+        self.options = {"icon_file":sys.path[0]+"/images/mini_breakfast.png",
+                        "blog_name":"tittendestages",
+                        "trigger_word":"",
+                        "rotation_time_reset":4}
+        self.last_time=0
+        self.rotate_counter=0
         
     def activate(self):        
         iface_called_plugin.activate(self)
@@ -29,14 +34,19 @@ class tdtnotify(iface_called_plugin):
                 u = urllib2.urlopen("http://api.tumblr.com/v2/blog/"+self.options['blog_name']+".tumblr.com/posts/photo?api_key=SyMOX3RGVS4OnK2bGWBcXNUfX34lnzQJY5FRB6uxpFqjEHz2SY")
 
                 j = json.load(u)
-                picurl = j['response']['posts'][0]['photos'][0]['original_size']['url'];
+                if (time.time()-self.last_time) > (3600*self.options["rotation_time_reset"]):
+                    self.rotate_counter = 0
+                picurl = j['response']['posts'][self.rotate_counter]['photos'][0]['original_size']['url'];
 
                 pic = urllib2.urlopen(picurl)
                 icon = self.localFile.name
+                self.localFile.seek(0)
                 self.localFile.truncate()
                 self.localFile.write(pic.read())
                 self.localFile.flush()
                 subprocess.call(["notify-send","--icon="+icon, msg + name])
+                self.last_time = time.time()
+                self.rotate_counter+=1
             except:
                 self.logger.error("TDT notify error "+str(sys.exc_info()[0]))
             
