0b1f439cefb111cddd18fc75e356a447a08c0901
Hannes Rauhe <hannes@scitivity.net>
2013-08-08 16:16:11 +0200

    *TDTnotify - only one temp file for the whole runtime - will be deleted afterwards

--- plugins/tdtnotify/__init__.py
+++ plugins/tdtnotify/__init__.py
@@ -13,6 +13,11 @@ class tdtnotify(iface_called_plugin):
         
     def activate(self):        
         iface_called_plugin.activate(self)
+        self.localFile = tempfile.NamedTemporaryFile()
+    
+    def deactivate(self):        
+        self.localFile.close()
+        iface_called_plugin.deactivate(self)
             
     def process_message(self,msg,addr,member_info):
         if sys.platform.startswith('linux'):    
@@ -27,11 +32,10 @@ class tdtnotify(iface_called_plugin):
                 picurl = j['response']['posts'][0]['photos'][0]['original_size']['url'];
 
                 pic = urllib2.urlopen(picurl)
-                localFile = tempfile.NamedTemporaryFile(delete=False)
-                icon = localFile.name
-                localFile.write(pic.read())
-                localFile.flush()
-                localFile.close()
+                icon = self.localFile.name
+                self.localFile.truncate()
+                self.localFile.write(pic.read())
+                self.localFile.flush()
                 subprocess.call(["notify-send","--icon="+icon, msg + name])
             except:
                 self.logger.error("TDT notify error "+str(sys.exc_info()[0]))
