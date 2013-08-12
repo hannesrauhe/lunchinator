5a925a9f07fcbb693e877f363d77fda20a54acb5
Hannes Rauhe <hannes@scitivity.net>
2013-08-12 14:49:48 +0200

    *About Plugins

---
+++ plugins/list_plugins.py
@@ -0,0 +1,46 @@
+from lunchinator.iface_plugins import *
+from yapsy.PluginManager import PluginManagerSingleton
+import gtk,gobject,urllib2,sys
+    
+class list_plugins(iface_gui_plugin):
+    ls = None
+    
+    def __init__(self):
+        super(list_plugins, self).__init__()
+        manager = PluginManagerSingleton.get()
+        self.ls = manager.app
+        #self.options = {"url":"http://155.56.69.85:1081/lunch_de.txt" }
+        
+    def activate(self):
+        iface_gui_plugin.activate(self)
+        
+    def deactivate(self):
+        iface_gui_plugin.deactivate(self)
+    
+    def create_widget(self):
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        textview = gtk.TextView()
+        textview.set_size_request(400,200)
+        textview.set_wrap_mode(gtk.WRAP_WORD)
+        textbuffer = textview.get_buffer()
+        sw.add(textview)
+        sw.show()
+        textview.show()
+        txt = ""
+        manager = PluginManagerSingleton.get()
+        for pluginInfo in manager.getAllPlugins():    
+            txt+=pluginInfo.name + " - "
+#            txt+=pluginInfo.path +" "
+#            txt+=pluginInfo.version +" "
+            txt+=str(pluginInfo.author) +", "
+#            txt+=pluginInfo.copyright +" "
+#            txt+=pluginInfo.website +" "
+            txt+=pluginInfo.description +" "
+#            txt+=pluginInfo.details + " "
+            txt+="\n\n"
+        textbuffer.set_text(txt)
+        return sw
+    
+    def add_menu(self,menu):
+        pass
