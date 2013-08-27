98305978283bb6832c2d85471fe9a5bad1b8625e
Hannes Rauhe <hannes.rauhe@sap.com>
2013-08-27 14:51:25 +0200

    webcam: option to disable proxy

--- plugins/webcam.py
+++ plugins/webcam.py
@@ -10,8 +10,9 @@ class webcam(iface_gui_plugin):
         manager = PluginManagerSingleton.get()
         self.ls = manager.app
         self.options = {"fallback_pic":sys.path[0]+"/images/webcam.jpg",
-#                        "pic_url":"http://lunchinator.de/files/webcam_dummy.jpg",
-                        "timeout":5}
+                        "pic_url":"http://lunchinator.de/files/webcam_dummy.jpg",
+                        "timeout":5,
+                        "no_proxy":False}
         
     def activate(self):
         iface_gui_plugin.activate(self)
@@ -20,7 +21,7 @@ class webcam(iface_gui_plugin):
         iface_gui_plugin.deactivate(self)
     
     def create_widget(self):
-        webcam = UpdatingImage(self.options["fallback_pic"],self.options["pic_url"],self.options["timeout"])
+        webcam = UpdatingImage(self.options["fallback_pic"],self.options["pic_url"],self.options["timeout"],self.options["no_proxy"])
         return webcam.gtkimage
     
     def add_menu(self,menu):
@@ -29,13 +30,11 @@ class webcam(iface_gui_plugin):
     
 
 class UpdatingImage():
-    pic_url = None
-    timeout = 5000
-    fallback_pic = None
-    def __init__(self,fallback_pic,pic_url,timeout):
+    def __init__(self,fallback_pic,pic_url,timeout,no_proxy):
         self.fallback_pic = fallback_pic
         self.pic_url = pic_url
         self.timeout = int(timeout)*1000
+        self.no_proxy = no_proxy
         try:     
             self.gtkimage = gtk.Image() 
             self.gtkimage.set_from_file(self.fallback_pic)
@@ -46,10 +45,13 @@ class UpdatingImage():
             
     def update(self): 
         try:
-            #todo disable proxy for now
-            proxy_handler = urllib2.ProxyHandler({})
-            opener = urllib2.build_opener(proxy_handler)
-            response=opener.open(self.pic_url)
+            response = None
+            if self.no_proxy:
+                proxy_handler = urllib2.ProxyHandler({})
+                opener = urllib2.build_opener(proxy_handler)
+                response=opener.open(self.pic_url)
+            else:
+                response = urllib2.urlopen(self.pic_url)
             loader=gtk.gdk.PixbufLoader()
             loader.write(response.read())
             loader.close()   
