884928c6573c6929ab298ca50f8eef5cf32c6f47
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2013-10-01 18:49:42 +0200

    fixed bug report gui

--- plugins/bug_report/__init__.py
+++ plugins/bug_report/__init__.py
@@ -11,7 +11,7 @@ class bug_report(iface_gui_plugin):
         iface_gui_plugin.deactivate(self)
     
     def create_widget(self, parent):
-        from bug_report_gui import bug_report_gui
+        from bug_report.bug_report_gui import bug_report_gui
         w = bug_report_gui(parent)
         return w.create_widget(parent)
             
