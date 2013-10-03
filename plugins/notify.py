24bd60c560a7eef1ef687a3a63d6bd5e660301f0
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2013-10-03 12:19:58 +0200

    using convenience functions in notify plugin

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,7 +1,6 @@
 from lunchinator.iface_plugins import iface_called_plugin
-import subprocess,sys,ctypes
-from lunchinator import log_exception, get_settings
-import os
+from lunchinator import get_settings
+from lunchinator.utilities import displayNotification, drawAttention
 
 class Notify(iface_called_plugin):    
     def __init__(self):
@@ -12,74 +11,17 @@ class Notify(iface_called_plugin):
         iface_called_plugin.activate(self)
             
     def process_message(self,msg,addr,member_info):
-        name = " ["+addr+"]"    
-        if sys.platform.startswith('linux'):
-            try:
-                icon = self.options["icon_file"]
-                if member_info.has_key("avatar"):
-                    icon = get_settings().get_avatar_dir()+"/"+member_info["avatar"]
-    #            print ["notify-send","--icon="+icon, msg + " [" + member_info["name"] + "]"]
-                if member_info.has_key("name"):
-                    name = " [" + member_info["name"] + "]"
-                subprocess.call(["notify-send","--icon="+icon, name, msg])
-            except:
-                log_exception("notify error: %s"%str(sys.exc_info()))
-        elif "darwin" in sys.platform:
-            try:
-                fh = open(os.path.devnull,"w")
-                subprocess.call(["terminal-notifier", "-title", "Lunchinator: %s" % name, "-message", msg], stdout=fh, stderr=fh)
-            except:
-                log_exception("error posting notification")
-        else:
-            self.incoming_call_win(msg,addr,member_info)
-            
-    def process_lunch_call(self,msg,ip,member_info):
-        if sys.platform.startswith('linux'):
-            self.incoming_call_linux(msg,ip,member_info)
-        else:
-            self.incoming_call_win(msg,ip,member_info)
-            
-    def incoming_call_linux(self,msg,addr,member_info):       
-        try:
-            subprocess.call(["eject", "-T", "/dev/cdrom"])
-        except:
-            log_exception("notify error: eject error (open)")
+        name = " ["+addr+"]"
+        icon = self.options["icon_file"]
+        if member_info.has_key("avatar"):
+            icon = get_settings().get_avatar_dir()+"/"+member_info["avatar"]
+        if member_info.has_key("name"):
+            name = " [" + member_info["name"] + "]"
         
-        try:
-            subprocess.call(["play", "-q", self.options["audio_file"]])    
-        except:
-            log_exception("notify error: sound error")
-    
-        try:
-            subprocess.call(["eject", "-T", "/dev/cdrom"])
-        except:
-            log_exception("notify error: eject error (close)")
+        displayNotification(name, msg, icon)
             
-    def incoming_call_mac(self,msg,addr,member_info):      
-        try:
-            subprocess.call(["drutil", "tray", "eject"])
-        except:
-            log_exception("notify error: eject error (open)")
-             
-        try:
-            subprocess.call(["afplay", self.options["audio_file"]])    
-        except:
-            log_exception("notify error: sound error")
-             
-        try:
-            subprocess.call(["drutil", "tray", "close"])
-        except:
-            log_exception("notify error: eject error (close)")
-    
-    def incoming_call_win(self,msg,addr,member_info):    
-        try:
-            ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
-        except:
-            log_exception("notify error: eject error (open)")
-        try:
-            ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
-        except:
-            log_exception("notify error: eject error (close)")
+    def process_lunch_call(self,_msg,_ip,_member_info):
+        drawAttention(self.options["audio_file"])
 
     def process_event(self,cmd,value,ip,member_info):
         pass
