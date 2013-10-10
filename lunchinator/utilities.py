7b46e45c30ddeeebb4a12b1a3bb39c5b6948eb44
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2013-10-10 19:06:48 +0200

    removing stupid auto-import

--- lunchinator/utilities.py
+++ lunchinator/utilities.py
@@ -1,9 +1,8 @@
 import subprocess,sys,ctypes
-from lunchinator import log_exception, get_settings, get_server, log_warning,\
+from lunchinator import log_exception, get_server, log_warning,\
     log_debug
 import os
 from lunchinator.iface_plugins import iface_called_plugin, iface_gui_plugin
-from Image import NONE
 import threading
 
 PLATFORM_OTHER = -1
