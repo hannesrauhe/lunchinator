6891ad02053d1d97f4f65089d0b32532126c2d59
Hannes <hannes@scitvity.net>
2013-10-13 19:09:13 +0200

    *timer for twitter pics - some exception handling, and resetting on changed settings

--- plugins/twitter_pics/__init__.py
+++ plugins/twitter_pics/__init__.py
@@ -7,20 +7,20 @@ from lunchinator.utilities import displayNotification, getValidQtParent
 import urllib2, tempfile, json, time, twitter, contextlib
 from get_access_token import get_access_token
 
-from PyQt4.QtCore import QThread, pyqtSignal
+from PyQt4.QtCore import QThread, pyqtSignal, QTimer
 
 class TwitterDownloadThread(QThread):
     success = pyqtSignal(QThread, tuple)
     unchanged = pyqtSignal(QThread, tuple)
     error = pyqtSignal(QThread, tuple)
     
-    def __init__(self, parent, twitter_api, screen_name, target, pic_url):
+    def __init__(self, parent, twitter_api, screen_name, target, old_pic_url):
         super(TwitterDownloadThread, self).__init__(parent)
         self.twitter_api = twitter_api
         self.screen_name = screen_name
         self.target = target
-        self.pic_url = None
-
+        self.old_pic_url = old_pic_url
+        
     def run(self):
         urls = []
         try:
@@ -38,19 +38,18 @@ class TwitterDownloadThread(QThread):
             self.error.emit(self, "Twitter")
             return       
         
-        if len(urls) and urls[0]!=self.pic_url:
-            self.pic_url=urls[0]
+        if len(urls) and urls[0]!=self.old_pic_url:
             try:
-                with contextlib.closing(urllib2.urlopen(self.pic_url[0])) as u:
+                with contextlib.closing(urllib2.urlopen(urls[0][0])) as u:
                     self.target.seek(0)
                     self.target.truncate()
                     self.target.write(u.read())
-                    self.success.emit(self, self.pic_url)
+                    self.success.emit(self, urls[0])
             except:
-                log_exception("Error while downloading %s"%str(self.pic_url))
-                self.error.emit(self, self.pic_url)
-        else:
-            self.unchanged.emit(self, self.pic_url)
+                log_exception("Error while downloading %s"%str(urls[0]))
+                self.error.emit(self, urls[0])
+        else:    
+            self.unchanged.emit(self, self.old_pic_url)
 
 class twitter_pics(iface_gui_plugin):
     def __init__(self):
@@ -59,23 +58,18 @@ class twitter_pics(iface_gui_plugin):
                         (("key","API Key"),""),
                         (("secret","API Secret"),""),
                         (("at_key","Access Token Key"),""),
-                        (("at_secret","Access Token Secret"),"")]
+                        (("at_secret","Access Token Secret", self.authenticate),""),
+                        (("polling_time","Polling Time", self.reset_timer),60)]
         self.localFile = None
-        self.pic_url = ("","")
+        self.old_pic_url = ("","")
+        self.timer = None
+        self.twitter_api = None
         
     def activate(self):        
         iface_gui_plugin.activate(self)
         self.localFile = tempfile.NamedTemporaryFile()
         self.forceDownload = False
-        if len(self.options["key"]) and len(self.options["secret"]) and len(self.options["at_key"]) and len(self.options["at_secret"]):
-            self.twitter_api = twitter.Api(consumer_key=self.options["key"],
-                         consumer_secret=self.options["secret"],
-                         access_token_key=self.options["at_key"],
-                         access_token_secret=self.options["at_secret"],
-                         debugHTTP=False)
-            self.download_pic(True)
-        else:
-            log_error("Twitter Pics: provide keys and secrets in settings")
+        self.authenticate()
     
     def deactivate(self):    
         if self.localFile:    
@@ -88,35 +82,58 @@ class twitter_pics(iface_gui_plugin):
         
         super(twitter_pics, self).create_widget(parent)
         self.imageLabel = ResizingImageLabel(parent, True, QSize(400,400))
+        self.timer = QTimer(self.imageLabel)
+        self.timer.timeout.connect(self.download_pic)
+        self.timer.setSingleShot(False)
+        self.timer.start(self.options["polling_time"]*1000)
         return self.imageLabel
-
-    def process_message(self,msg,addr,member_info):
-        pass
+    
+    def authenticate(self,old_v=None,new_v=None):
+        if len(self.options["key"]) and len(self.options["secret"]) and len(self.options["at_key"]) and len(self.options["at_secret"]):
+            try:
+                self.twitter_api = twitter.Api(consumer_key=self.options["key"],
+                             consumer_secret=self.options["secret"],
+                             access_token_key=self.options["at_key"],
+                             access_token_secret=self.options["at_secret"],
+                             debugHTTP=False)
+            except:
+                log_exception("Twitter Pics: authentication with twitter failed: check settings")
+            self.download_pic()
+        else:
+            log_error("Twitter Pics: provide keys and secrets in settings")
+            
+    def reset_timer(self,old_v=None,new_v=None):
+        self.timer.stop()
+        self.timer.start(self.options["polling_time"]*1000)
+          
+    def download_pic(self):
+        if self.twitter_api:
+            thread = TwitterDownloadThread(getValidQtParent(), self.twitter_api, self.options['twitter_user'], self.localFile, self.old_pic_url)
+            thread.success.connect(self.downloadedPicture)
+            thread.error.connect(self.errorDownloadingPicture)
+            thread.unchanged.connect(self.foundOldPicture)
+            thread.finished.connect(thread.deleteLater)
+            thread.start()
     
     def errorDownloadingPicture(self, thread, url):
         log_error("Error downloading picture from url %s" % str(url))
         thread.deleteLater()
         
     def downloadedPicture(self, _thread, pic_url):
-        self.pic_url = pic_url
+        self.old_pic_url = pic_url
         self.localFile.flush()
         displayNotification("Twitter", pic_url[1], self.localFile.name)
         self.imageLabel.setImage(self.localFile.name) 
         self.imageLabel.setToolTip(pic_url[1])  
         
     def foundOldPicture(self, _thread, pic_url):
-        log_debug("Twitter Pics: no new picture found")          
-          
-    def download_pic(self,force=False):
-        thread = TwitterDownloadThread(getValidQtParent(), self.twitter_api, self.options['twitter_user'], self.localFile, self.pic_url)
-        thread.success.connect(self.downloadedPicture)
-        thread.error.connect(self.errorDownloadingPicture)
-        thread.unchanged.connect(self.foundOldPicture)
-        thread.finished.connect(thread.deleteLater)
-        thread.start()
+        log_debug("Twitter Pics: no new picture found")  
             
     def process_lunch_call(self,msg,ip,member_info):
         pass
         
     def process_event(self,cmd,value,_,__):
         pass
+
+    def process_message(self,msg,addr,member_info):
+        pass        
