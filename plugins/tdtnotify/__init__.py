d37638a20e11fef71c76a8830460a6a1e477f8a2
Hannes <hannes@scitvity.net>
2014-01-20 16:03:50 +0100

    *Remote Pics: removed unnecessary startub error message *TDT Notify: removed all functionality which is provided by Remote Pics... is a category there now

--- plugins/tdtnotify/__init__.py
+++ plugins/tdtnotify/__init__.py
@@ -1,12 +1,13 @@
-from lunchinator.iface_plugins import iface_gui_plugin
+from lunchinator.iface_plugins import iface_called_plugin
 import subprocess, sys, ctypes
 from lunchinator import get_server, log_exception, log_warning, get_settings, convert_string, log_error
 from lunchinator.download_thread import DownloadThread
 from lunchinator.utilities import displayNotification, getValidQtParent
+from cStringIO import StringIO
 
-import urllib2, tempfile, json, time
+import urllib2, tempfile, json, time, contextlib, csv
 
-class tdtnotify(iface_gui_plugin):
+class tdtnotify(iface_called_plugin):
     def __init__(self):
         super(tdtnotify, self).__init__()
         self.options = {"icon_file":get_settings().get_lunchdir()+"/images/mini_breakfast.png",
@@ -14,46 +15,44 @@ class tdtnotify(iface_gui_plugin):
                         "trigger_word":"",
                         "polling_time":30}
         self.last_time=0
-        self.rotate_counter=0
+        self.rotate_counter=5
         self.pic_url = ""
         self.localFile = None
         self.forceDownload = False
         self.imageLabel = None
         
     def activate(self):        
-        iface_gui_plugin.activate(self)
+        iface_called_plugin.activate(self)
         self.last_time=0
         self.rotate_counter=0
         self.pic_url = ""
-        self.localFile = tempfile.NamedTemporaryFile()
-        self.extract_pic(True)
-        self.shared_dict["tdtnotify_file"] = self.localFile.name
     
     def deactivate(self):        
-        self.localFile.close()
-        del self.shared_dict["tdtnotify_file"]
-        iface_gui_plugin.deactivate(self)
-            
-    def create_widget(self, parent):
-        from lunchinator.resizing_image_label import ResizingImageLabel
-        from PyQt4.QtCore import QSize
+        iface_called_plugin.deactivate(self)
         
-        super(tdtnotify, self).create_widget(parent)
-        self.imageLabel = ResizingImageLabel(parent, True, QSize(400,400))
-        return self.imageLabel
-
     def process_message(self,msg,addr,member_info):
         pass
-    
-    def errorDownloadingPicture(self, thread, url):
-        log_error("Error downloading picture from url %s" % convert_string(url))
-        thread.deleteLater()
+            
+    def process_lunch_call(self,msg,ip,member_info):
+        pass
         
-    def downloadedPicture(self, _thread, _):
-        self.localFile.flush()
-        get_server().call("HELO_TDTNOTIFY_NEW_PIC "+self.pic_url)
-        displayNotification("TDT", "New picture", self.localFile.name)
-        self.imageLabel.setImage(self.localFile.name)
+    def process_event(self,cmd,value,_,__):
+        if cmd=="HELO_TDTNOTIFY_NEW_PIC" or (time.time()-self.last_time) > (60*self.options["polling_time"]):            
+            self.last_time = time.time()
+            if not cmd=="HELO_TDTNOTIFY_NEW_PIC":
+                get_server().call("HELO_TDTNOTIFY_POLL "+str(self.options["polling_time"]))
+                
+            self.forceDownload = value=="force"      
+            try:
+                getValidQtParent()
+            except:
+                log_warning("TDT Notify does not work without QT")
+                return
+            downloadThread = DownloadThread(getValidQtParent(), "http://api.tumblr.com/v2/blog/"+self.options['blog_name']+".tumblr.com/posts/photo?api_key=SyMOX3RGVS4OnK2bGWBcXNUfX34lnzQJY5FRB6uxpFqjEHz2SY")
+            downloadThread.success.connect(self.downloadedJSON)
+            downloadThread.error.connect(self.errorDownloadingJSON)
+            downloadThread.finished.connect(downloadThread.deleteLater)
+            downloadThread.start()
         
     def errorDownloadingJSON(self, thread, url):
         log_error("Error downloading JSON from url %s" % convert_string(url))
@@ -66,34 +65,8 @@ class tdtnotify(iface_gui_plugin):
         oldurl = self.pic_url
         self.pic_url = j['response']['posts'][self.rotate_counter]['photos'][0]['original_size']['url']
         if self.forceDownload or oldurl!=self.pic_url:
-            self.localFile.seek(0)
-            self.localFile.truncate()
-            thread = DownloadThread(getValidQtParent(), self.pic_url, self.localFile)
-            thread.success.connect(self.downloadedPicture)
-            thread.error.connect(self.errorDownloadingPicture)
-            thread.finished.connect(thread.deleteLater)
-            thread.start()
-          
-    def extract_pic(self,force=False):
-        self.forceDownload = force        
-        try:
-            getValidQtParent()
-        except:
-            log_warning("TDT Notify does not work without QT")
-            return
-        downloadThread = DownloadThread(getValidQtParent(), "http://api.tumblr.com/v2/blog/"+self.options['blog_name']+".tumblr.com/posts/photo?api_key=SyMOX3RGVS4OnK2bGWBcXNUfX34lnzQJY5FRB6uxpFqjEHz2SY")
-        downloadThread.success.connect(self.downloadedJSON)
-        downloadThread.error.connect(self.errorDownloadingJSON)
-        downloadThread.finished.connect(downloadThread.deleteLater)
-        downloadThread.start()
-            
-    def process_lunch_call(self,msg,ip,member_info):
-        pass
-        
-    def process_event(self,cmd,value,_,__):
-        if cmd=="HELO_TDTNOTIFY_NEW_PIC" or (time.time()-self.last_time) > (60*self.options["polling_time"]):            
-            self.last_time = time.time()
-            if not cmd=="HELO_TDTNOTIFY_NEW_PIC":
-                get_server().call("HELO_TDTNOTIFY_POLL "+str(self.options["polling_time"]))
-                
-            self.extract_pic(value=="force")
+            get_server().call("HELO_TDTNOTIFY_NEW_PIC "+self.pic_url)
+            with contextlib.closing(StringIO()) as strOut:
+                writer = csv.writer(strOut, delimiter = ' ', quotechar = '"')
+                writer.writerow([self.pic_url, "new picture", "TDT"])
+                get_server().call('HELO_REMOTE_PIC %s' % strOut.getvalue())
