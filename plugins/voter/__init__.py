403ac3d41426667f1b890944ecbd942beba48317
Hannes Rauhe <hannes@scitivity.net>
2014-04-17 10:11:19 +0200

    minor fixes for plugins voter and twitter

--- plugins/voter/__init__.py
+++ plugins/voter/__init__.py
@@ -1,6 +1,6 @@
 from lunchinator.iface_plugins import iface_gui_plugin
 from lunchinator import get_server, log_exception, log_error
-from PyQt4.QtGui import QTreeView, QWidget, QSortFilterProxyModel, QSizePolicy, QTableWidgetItem, QPushButton
+from PyQt4.QtGui import QTreeView, QWidget, QSortFilterProxyModel, QSizePolicy, QTableWidgetItem, QPushButton, QPalette, QColor
 from PyQt4.QtCore import Qt, QTime
 from ui_voter import Ui_Voter
 import json
@@ -9,9 +9,12 @@ class voter(iface_gui_plugin):
     def __init__(self):
         super(voter, self).__init__()
         self.w = None
+        
+        self.vote_count = {}
+        self.ip2vote = {}        
     
     def create_widget(self, parent):        
-        self.w = voterWidget(parent)            
+        self.w = voterWidget(parent, self.send_vote)            
         return self.w
     
     def process_event(self, cmd, value, ip, member_info):
@@ -21,73 +24,91 @@ class voter(iface_gui_plugin):
                 return
             vote = json.loads(value)
             if vote.has_key("time") and vote.has_key("place"):
-                self.w.add_vote(ip, vote["place"], vote["time"])
+                self.add_vote(ip, vote["place"], vote["time"])
             else:
                 log_error("Voter: Vote does not look valid: " + value)
+        
+    def add_vote(self, ip, vote_place, vote_time):
+        if self.ip2vote.has_key(ip):
+            # member has already voted, revoke old vote
+            old_vote = self.ip2vote[ip]
+            self.vote_count[ old_vote ] -= 1
+            if self.w:
+                self.w.update_table_row(old_vote[0],
+                                   old_vote[1],
+                                   self.vote_count[ old_vote])
+            if self.vote_count[ old_vote ] == 0:
+                self.vote_count.pop(old_vote)
+            
+        self.ip2vote[ip] = (vote_place, vote_time)
+        if self.vote_count.has_key(self.ip2vote[ip]):
+            self.vote_count[ self.ip2vote[ip] ] += 1
+            if self.w:
+                self.w.update_table_row(vote_place,
+                                   vote_time,
+                                   self.vote_count[ self.ip2vote[ip] ])
+        else:
+            self.vote_count[ self.ip2vote[ip] ] = 1  
+            if self.w: 
+                self.w.add_place_to_dropdown(vote_place)  
+                self.w.add_table_row(vote_place, vote_time)  
+            
+    
+    def send_vote(self, place, time):
+        vote_call = "HELO_VOTE "+json.dumps({"place": unicode(place), "time": unicode(time)})
+        get_server().call(vote_call)
 
 class voterWidget(QWidget):
-    def __init__(self, parent):
+    def __init__(self, parent, vote_clicked_callable):
         super(voterWidget, self).__init__(parent)
         self.ui = Ui_Voter()
         self.ui.setupUi(self)
-        
-        self.vote_count = {}
-        self.ip2vote = {}
+        self.send_vote = vote_clicked_callable
 
-        self.tmp = 0
-        
-    def send_vote(self, place, time):
-#         self.add_vote(self.tmp, place, time)
-#         self.tmp += 1
-        vote_call = "HELO_VOTE "+json.dumps({"place": unicode(place), "time": unicode(time)})
-        get_server().call(vote_call)
+    def clear_votes(self):
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(0)
         
     def vote_clicked(self):
-        self.send_vote(self.ui.comboBox.currentText(), self.ui.timeEdit.text())
+        if self.ui.comboBox.currentText():
+            self.send_vote(self.ui.comboBox.currentText(), self.ui.timeEdit.text())
         
     def tablevote_clicked(self, row, column):
-        print "click"
         vote_place = self.ui.tableWidget.item(row, 0).text()
-        vote_time = self.ui.tableWidget.item(row, 0).text()
+        vote_time = self.ui.tableWidget.item(row, 1).text()
         self.ui.comboBox.setCurrentIndex(self.ui.comboBox.findText(vote_place))
         self.ui.timeEdit.setTime(QTime.fromString(vote_time, "HH:mm"))
         
         self.send_vote(vote_place, vote_time)
         
-    def add_vote(self, ip, vote_place, vote_time):
-        if self.ip2vote.has_key(ip):
-            # member has already voted
-            old_vote = self.ip2vote[ip]
-            self.vote_count[ old_vote ] -= 1
-            self.update_table_row(old_vote[0],
-                                   old_vote[1],
-                                   self.vote_count[ old_vote])
-            
-        self.ip2vote[ip] = (vote_place, vote_time)
-        if self.vote_count.has_key(self.ip2vote[ip]):
-            self.vote_count[ self.ip2vote[ip] ] += 1
-            self.update_table_row(vote_place,
-                                   vote_time,
-                                   self.vote_count[ self.ip2vote[ip] ])
-        else:
-            self.add_place_to_dropdown(vote_place)
-            self.vote_count[ self.ip2vote[ip] ] = 1     
-            self.add_table_row(vote_place, vote_time)  
-        
     def add_table_row(self, vote_place, vote_time, vote_count=1):
-        rowIndex = self.ui.tableWidget.rowCount()
-        self.ui.tableWidget.setRowCount(rowIndex + 1)
-        self.ui.tableWidget.setItem(rowIndex, 0, QTableWidgetItem(vote_place))
-        self.ui.tableWidget.setItem(rowIndex, 1, QTableWidgetItem(vote_time))
-        self.ui.tableWidget.setItem(rowIndex, 2, QTableWidgetItem(str(vote_count)))
-        self.ui.tableWidget.setItem(rowIndex, 3, QTableWidgetItem("Vote!"))
+        insertIndex = self.ui.tableWidget.rowCount()
+        #for rowIndex in range(0, self.ui.tableWidget.rowCount()):
+            
+                    
+        self.ui.tableWidget.insertRow(insertIndex)
+#         self.ui.tableWidget.setRowCount(rowIndex + 1)
+        self.ui.tableWidget.setItem(insertIndex, 0, QTableWidgetItem(vote_place))
+        self.ui.tableWidget.setItem(insertIndex, 1, QTableWidgetItem(vote_time))
+        self.ui.tableWidget.setItem(insertIndex, 2, QTableWidgetItem(str(vote_count)))
+        self.ui.tableWidget.setItem(insertIndex, 3, QTableWidgetItem("Vote!"))
         self.ui.tableWidget.resizeColumnsToContents()
-        return rowIndex
+        self.ui.tableWidget.sortItems(1)
+        self.ui.tableWidget.sortItems(0)
+        self.ui.tableWidget.sortItems(2, Qt.DescendingOrder)
+        return insertIndex
         
     def update_table_row(self, vote_place, vote_time, vote_count):
         for rowIndex in range(0, self.ui.tableWidget.rowCount()):
             if self.ui.tableWidget.item(rowIndex, 0).text() == vote_place and self.ui.tableWidget.item(rowIndex, 1).text() == vote_time:
-                self.ui.tableWidget.setItem(rowIndex, 2, QTableWidgetItem(str(vote_count)))
+                if vote_count > 0:
+                    self.ui.tableWidget.setItem(rowIndex, 2, QTableWidgetItem(str(vote_count)))
+                else:
+                    self.ui.tableWidget.removeRow(rowIndex)
+                break
+        self.ui.tableWidget.sortItems(1)
+        self.ui.tableWidget.sortItems(0)
+        self.ui.tableWidget.sortItems(2, Qt.DescendingOrder)
         
     def add_place_to_dropdown(self, place):
         self.ui.comboBox.addItem(place)
@@ -95,5 +116,8 @@ class voterWidget(QWidget):
         
 
 if __name__ == "__main__":
+    def call_dummy(place,time):
+        pass
+    
     from lunchinator.iface_plugins import iface_gui_plugin
-    iface_gui_plugin.run_standalone(lambda window : voterWidget(window))
+    iface_gui_plugin.run_standalone(lambda window : voterWidget(window,call_dummy))
--- plugins/voter/ui_voter.py
+++ plugins/voter/ui_voter.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'voter.ui'
 #
-# Created: Wed Apr 16 19:27:02 2014
+# Created: Thu Apr 17 13:08:03 2014
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,7 +26,7 @@ except AttributeError:
 class Ui_Voter(object):
     def setupUi(self, Voter):
         Voter.setObjectName(_fromUtf8("Voter"))
-        Voter.resize(466, 284)
+        Voter.resize(445, 268)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -34,23 +34,6 @@ class Ui_Voter(object):
         Voter.setSizePolicy(sizePolicy)
         self.gridLayout = QtGui.QGridLayout(Voter)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.label = QtGui.QLabel(Voter)
-        self.label.setMaximumSize(QtCore.QSize(91, 16777215))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.comboBox = QtGui.QComboBox(Voter)
-        self.comboBox.setEditable(True)
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
-        self.gridLayout.addWidget(self.comboBox, 0, 1, 1, 1)
-        self.label_2 = QtGui.QLabel(Voter)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
-        self.timeEdit = QtGui.QTimeEdit(Voter)
-        self.timeEdit.setObjectName(_fromUtf8("timeEdit"))
-        self.gridLayout.addWidget(self.timeEdit, 0, 3, 1, 1)
-        self.pushButton = QtGui.QPushButton(Voter)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.gridLayout.addWidget(self.pushButton, 0, 4, 1, 1)
         self.tableWidget = QtGui.QTableWidget(Voter)
         self.tableWidget.setObjectName(_fromUtf8("tableWidget"))
         self.tableWidget.setColumnCount(4)
@@ -67,19 +50,34 @@ class Ui_Voter(object):
         item = QtGui.QTableWidgetItem()
         item.setTextAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter|QtCore.Qt.AlignCenter)
         self.tableWidget.setHorizontalHeaderItem(3, item)
-        self.gridLayout.addWidget(self.tableWidget, 1, 0, 1, 5)
+        self.gridLayout.addWidget(self.tableWidget, 1, 0, 1, 6)
+        self.label = QtGui.QLabel(Voter)
+        self.label.setMaximumSize(QtCore.QSize(91, 16777215))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.label_2 = QtGui.QLabel(Voter)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 0, 3, 1, 1)
+        self.timeEdit = QtGui.QTimeEdit(Voter)
+        self.timeEdit.setObjectName(_fromUtf8("timeEdit"))
+        self.gridLayout.addWidget(self.timeEdit, 0, 4, 1, 1)
+        self.pushButton = QtGui.QPushButton(Voter)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 0, 5, 1, 1)
+        self.comboBox = QtGui.QComboBox(Voter)
+        self.comboBox.setMinimumSize(QtCore.QSize(180, 0))
+        self.comboBox.setEditable(True)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.gridLayout.addWidget(self.comboBox, 0, 1, 1, 1)
 
         self.retranslateUi(Voter)
         QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), Voter.vote_clicked)
-        QtCore.QObject.connect(Voter, QtCore.SIGNAL(_fromUtf8("signal1(int)")), self.tableWidget.insertRow)
+        QtCore.QObject.connect(self.tableWidget, QtCore.SIGNAL(_fromUtf8("cellClicked(int,int)")), Voter.tablevote_clicked)
         QtCore.QMetaObject.connectSlotsByName(Voter)
 
     def retranslateUi(self, Voter):
         Voter.setWindowTitle(_translate("Voter", "Form", None))
-        self.label.setText(_translate("Voter", "Place:", None))
-        self.label_2.setText(_translate("Voter", "Time:", None))
-        self.timeEdit.setDisplayFormat(_translate("Voter", "HH:mm", None))
-        self.pushButton.setText(_translate("Voter", "Vote", None))
+        self.tableWidget.setSortingEnabled(False)
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("Voter", "Place", None))
         item = self.tableWidget.horizontalHeaderItem(1)
@@ -88,4 +86,8 @@ class Ui_Voter(object):
         item.setText(_translate("Voter", "# Votes", None))
         item = self.tableWidget.horizontalHeaderItem(3)
         item.setText(_translate("Voter", "Vote", None))
+        self.label.setText(_translate("Voter", "Place:", None))
+        self.label_2.setText(_translate("Voter", "Time:", None))
+        self.timeEdit.setDisplayFormat(_translate("Voter", "HH:mm", None))
+        self.pushButton.setText(_translate("Voter", "Vote", None))
 
