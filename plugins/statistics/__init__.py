f20d990dad593bcfe5a513b8ca72ac111741dc41
Hannes Rauhe <hannes@scitivity.net>
2014-05-31 21:25:07 +0200

    moved reusable DB stuff to iface_plugin
    *by calling add_supported_dbms in __init__ of a plugin the db
    functionality of a plugin gets activated
    *a class that handles statements for a special DBMS is then
    reinitialized every time a db connection changes

--- plugins/statistics/__init__.py
+++ plugins/statistics/__init__.py
@@ -2,7 +2,6 @@ from lunchinator.iface_plugins import *
 from lunchinator import get_server, log_debug,\
     get_settings, get_db_connection, log_warning, \
     get_notification_center, get_peers
-import threading
 
 class statistics(iface_called_plugin):
     def __init__(self):
@@ -11,98 +10,54 @@ class statistics(iface_called_plugin):
                           get_settings().get_available_db_connections(),
                           self.connect_to_db),
                          get_settings().get_default_db_connection())]
-        self.connectionPlugin = None
-        self.connect_lock = threading.Lock()
+        self.add_supported_dbms("SQLite Connection", statistics_sqlite)
+        self.add_supported_dbms("HANA Connection", statistics_hana)
     
     def activate(self):
         iface_called_plugin.activate(self)
-        get_notification_center().connectDBSettingChanged(self.connect_to_db)
-        get_notification_center().connectDBConnReady(self.connect_to_db)
         get_notification_center().connectPeerAppended(self.peer_update)
         get_notification_center().connectPeerUpdated(self.peer_update)
         
     def deactivate(self):
-        get_notification_center().disconnectDBSettingChanged(self.connect_to_db)
-        get_notification_center().disconnectDBConnReady(self.connect_to_db)
         get_notification_center().disconnectPeerAppended(self.peer_update)
         get_notification_center().disconnectPeerUpdated(self.peer_update)
-        iface_called_plugin.deactivate(self)
+        iface_called_plugin.deactivate(self)    
         
-    def connect_to_db(self, changedDBConn = None):
-        with self.connect_lock:
-            if self.connectionPlugin and changedDBConn and changedDBConn != self.options["db_connection"]:
-                return
-            dbPlugin, plugin_type = get_db_connection(self.options["db_connection"])
-            
-            if not dbPlugin:
-                log_error("Statistics: DB  connection %s not available: Activate a DB Connection plugin and check settings"
-                          %(self.options["db_connection"]))
-                return False
-            
-            if plugin_type == "SQLite Connection":
-                self.connectionPlugin = statistics_sqlite(dbPlugin)
-            elif plugin_type == "HANA Connection":
-                self.connectionPlugin = statistics_hana(dbPlugin)
-            else:
-                self.connectionPlugin = None
-                return False
-                
-            log_debug("Statistics: Using DB Connection ",plugin_type)
-                        
-            return True
-        
-    def is_db_ready(self):
-        return self.connectionPlugin and self.connectionPlugin.isOpen()
+    def processes_events_immediately(self):
+        return True    
     
     def process_message(self,msg,addr,member_info):
         if self.is_db_ready():
-            self.connectionPlugin.insert_call("msg", msg, addr)
+            self.specialized_db_conn().insert_call("msg", msg, addr)
         else:
             log_warning("Statistics: DB not ready -- cannot process message")
             
     def process_lunch_call(self,msg,ip,member_info):
         if self.is_db_ready():
-            self.connectionPlugin.insert_call("lunch", msg, ip)
+            self.specialized_db_conn().insert_call("lunch", msg, ip)
         else:
             log_warning("Statistics: DB not ready -- cannot process lunch_call")
     
     def process_event(self,cmd,value,ip,member_info):
         if self.is_db_ready():
-            self.connectionPlugin.insert_call(cmd, value, ip)
+            self.specialized_db_conn().insert_call(cmd, value, ip)
         else:
             log_warning("Statistics: DB not ready -- cannot process event")
             
     def peer_update(self, peerID, peerInfo):        
         if self.is_db_ready():
-            self.connectionPlugin.insert_members("ip", peerInfo["name"], \
+            self.specialized_db_conn().insert_members("ip", peerInfo["name"], \
                                  peerInfo["avatar"], peerInfo["next_lunch_begin"], peerInfo["next_lunch_end"])
         else:
             log_warning("Statistics: DB not ready -- cannot store member data")
-            
-class statistics_dbase(object):
-    def __init__(self, newconn):
-        self.dbConn = newconn
-        
-        try:
-            self.migrate()
-        except:
-            log_exception("Problem while migrating dataset to new version")
-    
-    def isOpen(self):
-        if self.dbConn:
-            return self.dbConn.isOpen()
-        return False
-    
-    def migrate(self):
-        raise NotImplementedError("Migration not implemented")
 
-class statistics_sqlite(statistics_dbase):
+class statistics_sqlite(db_for_plugin_iface):
     version_schema = "CREATE TABLE statistics_version (commit_count INTEGER, migrate_time INTEGER)"
     messages_schema = "CREATE TABLE statistics_messages (m_id INTEGER PRIMARY KEY AUTOINCREMENT, \
             mtype TEXT, message TEXT, sender TEXT, senderIP TEXT, rtime INTEGER)"
     members_schema = "CREATE TABLE statistics_members (peerID TEXT, name TEXT, avatar TEXT, lunch_begin TEXT, lunch_end TEXT, rtime INTEGER)"
     
-    def migrate(self):                   
+    def init_db(self):                   
         if not self.dbConn.existsTable("statistics_version"):
             self.dbConn.execute(self.version_schema)
             if self.dbConn.existsTable("messages"):
@@ -131,13 +86,13 @@ class statistics_sqlite(statistics_dbase):
     def get_newest_members_data(self):    
         return self.dbConn.query("SELECT peerID,name,avatar,lunch_begin,lunch_end,MAX(rtime) FROM statistics_members GROUP BY peerID")
     
-class statistics_hana(statistics_dbase):
+class statistics_hana(db_for_plugin_iface):
     messages_schema = "CREATE INSERT ONLY COLUMN TABLE messages ( \
             mtype VARCHAR(100), message VARCHAR(5000), sender VARCHAR(100), rtime SECONDDATE, receiver VARCHAR(100))";
     members_schema = "CREATE COLUMN TABLE members (IP VARCHAR(15), name VARCHAR(255), \
             avatar VARCHAR(255), lunch_begin VARCHAR(5), lunch_end VARCHAR(5), rtime SECONDDATE)"; 
     
-    def migrate(self):
+    def init_db(self):
         if not self.dbconn.existsTable("members"):
             self.dbconn.execute(self.members_schema)
         if not self.dbconn.existsTable("messages"):
