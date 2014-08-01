954c003881d59c08d542ec4f7b53a732bdab4f38
Hannes Rauhe <hannes@scitivity.net>
2014-08-01 15:34:32 +0200

    WebRelay fixes

--- plugins/webrelay/__init__.py
+++ plugins/webrelay/__init__.py
@@ -1,6 +1,6 @@
 from lunchinator.plugin import iface_called_plugin
 from lunchinator import log_error
-import urllib2, contextlib, base64, json
+import urllib, urllib2, contextlib, base64, json
 
 class webrelay(iface_called_plugin):        
     def __init__(self):
@@ -8,19 +8,22 @@ class webrelay(iface_called_plugin):
         self.options = [(("no_proxy", "Don't use proxy server"),False),
                         ((u"server",u"Server"),""),
                         ((u"callURL", u"URL to open when call comes in"),
-                         "/messages/addMessage.xsjs?message=$msg$&name=$name$&id=$id$"),
+                         "/webrelay/addMessage.xsjs?message=$msg$&name=$name$&id=$id$"),
                         ((u"http_user", u"User for HTTP Auth"), ""),
                         ((u"http_pass", u"Password for HTTP Auth"),"")]
         
     def process_message(self, msg, addr, member_info):
 #         print member_info
-        self._registerCall({"msg":msg, "id":member_info[u"ID"] , "name":member_info[u"name"]})
+        if self.options[u"server"]:
+            self._registerCall({"msg":msg, "id":member_info[u"ID"] , "name":member_info[u"name"]})
+        else:
+            log_error("WebRelay: please configure a server in Settings")
         
     def _registerCall(self, infos):
         call_url = self.options[u"callURL"]       
         
         for var in ["msg","name","id"]:
-            call_url = call_url.replace("$%s$"%var, infos[var]) 
+            call_url = call_url.replace("$%s$"%var, urllib.quote_plus(infos[var])) 
 
         call_url = self.options[u"server"] + call_url
         
@@ -50,6 +53,8 @@ class webrelay(iface_called_plugin):
                 resp = json.loads(response)
             except:
                 log_error("Invalid response from webserver after relaying call: "+response)
-            
+                
+            if not resp["success"]:
+                log_error("Webrelay: negative response from webserver after relaying call: "+response)
             
             
