f683bc867657233e76d577a91fd988cd57b3d2f0
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2014-08-24 08:33:47 +0200

    fixed more cli problems (more to follow for sure)

--- lunchinator/log/notification_handler.py
+++ lunchinator/log/notification_handler.py
@@ -5,7 +5,8 @@ class NotificationLogHandler(Handler):
     def __init__(self):
         # handler is old-style class in older Python versions
         Handler.__init__(self)
-        self._buf = deque(maxlen=100)
+        self._maxLen = 100
+        self._buf = deque(maxlen=self._maxLen)
     
     def emit(self, record):
         from lunchinator import get_notification_center
@@ -18,6 +19,7 @@ class NotificationLogHandler(Handler):
             return list(self._buf)
         
     def setCacheSize(self, size):
-        if self._buf.maxlen != size:
+        if self._maxLen != size:
             with self.lock:
                 self._buf = deque(self._buf, maxlen=size)
+                self._maxLen = size
