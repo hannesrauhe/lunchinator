8a83941c68e3cd8208a6b9d4d1f7688f3b2e5ca2
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2014-09-06 12:49:04 +0200

    removed lunch statistics stuff, added commit method (issue #237)

--- plugins/db_SQLite/__init__.py
+++ plugins/db_SQLite/__init__.py
@@ -4,17 +4,6 @@ from lunchinator import get_server, get_settings
 from db_SQLite.multithreaded_sqlite import MultiThreadSQLite
  
 class db_SQLite(iface_db_plugin):
-    VERSION_TABLE = "DB_VERSION"
-    DATABASE_VERSION_EMPTY = 0
-    DATABASE_VERSION_DEFAULT_STATISTICS = 1
-    DATABASE_VERSION_LUNCH_STATISTICS = 2
-    
-    version_schema = "CREATE TABLE \"%s\" (VERSION INTEGER)" % VERSION_TABLE 
-    lunch_soup_schema =    "CREATE TABLE LUNCH_SOUP    (DATE DATE, NAME TEXT, ADDITIVES TEXT, LAST_UPDATE DATE)" 
-    lunch_main_schema =    "CREATE TABLE LUNCH_MAIN    (DATE DATE, NAME TEXT, ADDITIVES TEXT, LAST_UPDATE DATE)" 
-    lunch_side_schema =    "CREATE TABLE LUNCH_SIDE    (DATE DATE, NAME TEXT, ADDITIVES TEXT, LAST_UPDATE DATE)" 
-    lunch_dessert_schema = "CREATE TABLE LUNCH_DESSERT (DATE DATE, NAME TEXT, ADDITIVES TEXT, LAST_UPDATE DATE)"
-      
     def __init__(self):
         super(db_SQLite, self).__init__()
         self.options=[("sqlite_file", os.path.join(get_settings().get_main_config_dir(),"lunchinator.sq3"))]
--- plugins/db_SQLite/multithreaded_sqlite.py
+++ plugins/db_SQLite/multithreaded_sqlite.py
@@ -22,6 +22,11 @@ class MultiThreadSQLite(threading.Thread, lunch_db):
                 res.put('--no more--')
                 description.put('--no more--')
                 break
+            if req=='--commit--':
+                cnx.commit()
+                res.put('--no more--')
+                description.put('--no more--')
+                continue
             try:
                 cursor.execute(req, arg)
                 description.put(cursor.description)
@@ -44,7 +49,10 @@ class MultiThreadSQLite(threading.Thread, lunch_db):
     def close(self, logger):
         self.execute(logger, '--close--')
         self.is_open = False
-        
+
+    def commit(self, logger):
+        self.execute(logger, '--commit--')
+            
     def _execute(self, logger, query, wildcards, returnResults=True, commit=False, returnHeader=False):            
         if not self.is_open:
             raise Exception("not connected to a database")
@@ -80,28 +88,6 @@ class MultiThreadSQLite(threading.Thread, lunch_db):
     def existsTable(self, logger, tableName):
         result = self.query(logger, "select sql from sqlite_master where type = 'table' and upper(name) = '%s'" % tableName.upper())
         return result != None and len(result) > 0   
-    
-    '''Lunch Statistics'''
-    def lastUpdateForLunchDay(self, logger, date, tableName):
-        sql="SELECT LAST_UPDATE FROM %s WHERE DATE=%s" % (self.get_table_name(logger, tableName), self.get_formatted_date(logger, date))
-        tuples = self.query(logger, sql)
-        if tuples == None or len(tuples) == 0:
-            logger.debug("%s -> None", sql)
-            return None
-        else:
-            logger.debug("%s -> %s", sql, tuples)
-            return self.parse_result_date(logger, tuples[0][0])
-        
-    def insertLunchPart(self, logger, date, textAndAdditivesList, update, table):
-        sql=None
-        if update:
-            sql="DELETE FROM %s WHERE DATE=%s" % (self.get_table_name(logger, table), self.get_formatted_date(logger, date))
-            logger.debug(sql)
-            self.executeNoCommit(logger, sql)
-        for textAndAdditives in textAndAdditivesList:
-            sql="INSERT INTO %s VALUES(%s, ?, ?, %s)" % (self.get_table_name(logger, table), self.get_formatted_date(logger, date), self.get_formatted_date(logger, date.today()))
-            logger.debug("%s, %s, %s", sql, textAndAdditives[0], textAndAdditives[1])
-            self.executeNoCommit(logger, sql, textAndAdditives[0], textAndAdditives[1])            
 
     def get_table_name(self, _logger, baseName):
         return "\"%s\"" % baseName    
