2141ac06bddc5fba43d1c6692a86ed64217af852
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2014-09-17 12:14:32 +0200

    fixed auto update for Mac (issue #248)

--- plugins/online_update/appupdate/mac_update.py
+++ plugins/online_update/appupdate/mac_update.py
@@ -46,7 +46,7 @@ class MacUpdateHandler(GPGUpdateHandler):
                 self._ui.setInteractive(False)
                 
                 self._setStatus("Searching for latest version...")
-                dt = DownloadThread(getValidQtParent(), self.logger, "https://releases.gpgtools.org/nightlies/macgpg2/appcast.xml")
+                dt = DownloadThread(getValidQtParent(), self.logger, "https://gpgtools.org/releases/macgpg2/appcast.xml")
                 dt.success.connect(partial(self._install_gpg_finished, 0))
                 dt.error.connect(partial(self._install_gpg_failed, 0))
                 dt.start()
