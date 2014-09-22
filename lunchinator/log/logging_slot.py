993d2ce84601df7d0d4f0f856fd397a0d502c792
Cornelius Ratsch <bc002@ix.urz.uni-heidelberg.de>
2014-09-22 09:59:53 +0200

    removing extra arguments to loggingSlot (issue #264)

--- lunchinator/log/logging_slot.py
+++ lunchinator/log/logging_slot.py
@@ -1,18 +1,20 @@
 from PyQt4.QtCore import pyqtSlot
 from functools import wraps
-import types
+import types, inspect
 
 def loggingSlot(*args, **kwargs):
     if len(args) == 0 or isinstance(args[0], types.FunctionType):
         args = []
     def slotdecorator(func):
         from lunchinator.log import getCoreLogger
-        
+        fArgs, _fVarargs, _fKeywords, _fDefaults = inspect.getargspec(func)
+        maxNumArgs = len(fArgs)
+                
         @pyqtSlot(*args, **kwargs)
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                func(*args, **kwargs)
+                func(*(args[:maxNumArgs - len(kwargs)]), **kwargs)
             except:
                 if len(args) > 0 and hasattr(args[0], "logger"):
                     logger = args[0].logger
